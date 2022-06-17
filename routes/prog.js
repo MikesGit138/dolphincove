48716f5e12c058051c1131e04ad1384ca67f30e2
MikesGit138 <michael138leighton@gmail.com>
2022-06-17 00:25:29 -0500

    view programs

--- routes/prog.js
+++ routes/prog.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
+const DB = require('../lib/db')
 
 router.get('/programs', (req,res)=>{
-	res.render('prog', {title: 'Programs'})
+  let sql = 'SELECT * FROM dolcove.programs'
+	let query = DB.query(sql, (err, rows)=>{
+	  res.render('prog', {
+		title: 'Programs',
+		programs: rows
+	})
+		
+	})
 })
 module.exports = router
