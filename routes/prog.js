8678c283003c0dfbc075f704668018f0dc034074
MikesGit138 <michael138leighton@gmail.com>
2022-06-18 11:03:47 -0500

    styling changes

--- routes/prog.js
+++ routes/prog.js
@@ -9,8 +9,17 @@ router.get('/programs', (req,res)=>{
 	  res.render('prog', {
 		title: 'Programs',
 		programs: rows
+		})
 	})
-		
-	})
+})
+
+router.get('/book/:id',(req,res)=> {
+    const id = req.params.id
+    let sql = `SELECT * FROM dolcove.programs WHERE id = ${id}`
+    let query = DB.query(sql, (err, result) =>{
+        if (err) throw err
+        res.send('this bookings page is for ' + id)
+    
+    })
 })
 module.exports = router
