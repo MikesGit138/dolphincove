61786b5c765fa1807cda5d8416dc10e3a7dc4bb2
MikesGit138 <michael138leighton@gmail.com>
2022-06-18 23:26:59 -0500

    booking changes

--- routes/prog.js
+++ routes/prog.js
@@ -18,8 +18,24 @@ router.get('/book/:id',(req,res)=> {
     let sql = `SELECT * FROM dolcove.programs WHERE id = ${id}`
     let query = DB.query(sql, (err, result) =>{
         if (err) throw err
-        res.send('this bookings page is for ' + id)
-    
+        res.render('book', {
+          title: 'Book'
+          })
     })
 })
+
+router.post('/addclient', (req,res)=>{
+  let data = {
+    fname: req.body.fname,
+    lname: req.body.lname,
+    email: req.body.email,
+    phone_no: req.body.tel
+  }
+  let sql = 'INSERT INTO clients SET ?'
+  let query = DB.query(sql, data, (err, result) =>{
+    if (err) throw err
+    res.send('new client made')
+  })
+})
+
 module.exports = router
